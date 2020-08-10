2a5f099a15c9515c72c106c6b26915b0f4774784
nicolasariza <nicolas_ariza@outlook.com>
2020-08-10 00:26:03 -0500

    footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 import { faAd } from '@fortawesome/free-solid-svg-icons';
 import { faHandsHelping } from '@fortawesome/free-solid-svg-icons';
 import { faChevronCircleDown } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -15,6 +17,8 @@ export class HomeComponent implements OnInit {
   faAd = faAd;
   faHandsHelping = faHandsHelping;
   faChevronCircleDown = faChevronCircleDown;
+  faInstagram = faInstagram;
+  faWhatsapp = faWhatsapp;
 
   constructor() { }
 
